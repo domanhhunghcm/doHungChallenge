cc01caa012c17e443b697508d1cccfd7bc618084
domanhhunghcm <domanhhunghcm@gmail.com>
2022-07-11 21:16:15 +0900

    change auth

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'verify' => true,
+    'register' => false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/postController', [postController::class, 'create'])->name('create.post');
